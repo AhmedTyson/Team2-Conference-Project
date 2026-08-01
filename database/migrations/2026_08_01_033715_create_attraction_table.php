72e12e9e6d48af044d597925e1fc357b1987543b
AhmedTyson <AhmedTyson@users.noreply.github.com>
2026-08-02 00:13:37 +0300

    feat: create attractions migration table with category and destination foreign keys

--- database/migrations/2026_08_01_033715_create_attraction_table.php
+++ database/migrations/2026_08_01_033715_create_attraction_table.php
@@ -15,8 +15,6 @@ return new class extends Migration
             $table->id();
             $table->foreignId('category_id')->constrained()->onDelete('cascade');
             $table->foreignId('destination_id')->constrained()->onDelete('cascade');
-            $table->foreignId('category_id')->constrained()->onDelete('cascade');
-
 
             $table->string('name');
             $table->string('description');
