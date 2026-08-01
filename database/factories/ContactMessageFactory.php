814d22c640eaa80230b208d5220c5a2ff15c2ef5
lojy-khaled <logykhaled019@gmail.com>
2026-08-01 13:35:50 +0300

    puch

---
+++ database/factories/ContactMessageFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\ContactMessage;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<ContactMessage>
+ */
+class ContactMessageFactory extends Factory
+{
+    protected $model = ContactMessage::class;
+
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name(),
+            'email' => fake()->safeEmail(),
+            'subject' => fake()->sentence(4),
+            'message' => fake()->paragraph(),
+            'status' => fake()->randomElement(['new', 'read', 'replied']),
+        ];
+    }
+}
