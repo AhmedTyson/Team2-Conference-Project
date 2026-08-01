b507f475e7b225186003c643d0ceb3ddf247f2f0
lojy-khaled <logykhaled019@gmail.com>
2026-08-01 21:11:37 +0300

    puch

---
+++ routes/api.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\AuthController;
+
+
+// Public routes 
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+// verification email 
+Route::get('/email/verify/{id}/{hash}', [AuthController::class, 'verifyEmail'])
+    ->middleware(['signed'])
+    ->name('verification.verify');
+
+// routes (must be logged in)
+Route::middleware(['auth:api'])->group(function () {
+    Route::get('/user', [AuthController::class, 'me']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/email/verify-notice', [AuthController::class, 'verificationNotice'])
+        ->name('verification.notice');
+    Route::post('/email/resend', [AuthController::class, 'resendVerificationEmail'])
+        ->middleware(['throttle:6,1'])
+        ->name('verification.resend');
+});
