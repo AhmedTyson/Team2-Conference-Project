50f5294680afa4f4e05a88fcb028f905ea9b3378
fady11336-cloud <fady11336@gmail.com>
2026-08-01 16:58:44 +0300

    Added migrations

---
+++ database/migrations/2026_08_01_024138_create_hotels_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('hotels', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('address');
+            $table->decimal('price_per_night',10,2);
+            
+            $table->decimal('rating',2,1)->nullable();
+            $table->tinyInteger('stars');
+            $table->string('image')->nullable();
+
+            $table->foreignId('destination_id')->constrained()->cascadeOnDelete();
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('hotels');
+    }
+};
