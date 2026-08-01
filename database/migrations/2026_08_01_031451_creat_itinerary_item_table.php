ec7854d268a8745f9b6154ddd14976bee07bc813
adham ahmed <amradhmahmd@gmail.com>
2026-08-01 07:14:52 +0300

    Add migration files for settings, hotels, flights, reviews, contact messages, itinerary items, attractions, AI recommendations, and categories

---
+++ database/migrations/2026_08_01_031451_creat_itinerary_item_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('itinerary_item',function (Blueprint $table){
+            $table->id();
+            $table->decimal('day_number');
+            $table->decimal('item_order');
+            $table->string('type');
+            $table->string('time_slot');  
+            $table->string('title');           
+            $table->string('notes');   
+            $table->decimal('estimated_cost');       
+            $table-> timestamps();                          
+        });                                       
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('itinerary_item');
+    }
+};
---
+++ database/migrations/2026_08_01_035221_create_ai_recommendation_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ai_recommendation', function (Blueprint $table) {
+            $table->id();
+            $table->string('prompt_text');
+            $table->string('response_text');
+            $table->string('generated_it');
+            $table->string('model_used');
+            $table->string('tokens_used');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ai_recommendation');
+    }
+};
