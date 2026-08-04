be62fe54208b3630730a71c685b4ddd06364576c
AhmedTyson <AhmedTyson@users.noreply.github.com>
2026-08-04 13:47:17 +0300

    Update User model and factory for phone and profile_image

--- app/Models/User.php
+++ app/Models/User.php
@@ -28,6 +28,7 @@ class User extends Authenticatable implements JWTSubject
     protected $fillable = [
         'name',
         'email',
+        'phone',
         'password',
         'profile_image',
     ];
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -27,6 +27,7 @@ class UserFactory extends Factory
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
+            'phone' => fake()->phoneNumber(),
             'email_verified_at' => now(),
             'password' => Hash::make('password'),
             'profile_image' => null,
